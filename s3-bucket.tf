505e0780c161a6308804db5a08b10d36936f1f75
Hector Franco Ramos <hector.francoramos@santander.co.uk>
2021-06-29 14:17:51 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,5 @@
-module "s3_bucket" {
-  source = "terraform-aws-modules/s3-bucket/aws"
-
-  bucket = "my-s3-bucket"
+module "s3-bucket" {
+  source  = "app.terraform.io/hframos-training/s3-bucket/aws"
+  version = "2.2.0"
   bucket_prefix = "hframos"
-  acl    = "private"
-
-  versioning = {
-    enabled = true
   }
-
-}
